c964e44d1f0cc1cef0e7d68ff8686cc613d653f1
Biricz Bence <71565433+BenceBiricz@users.noreply.github.com>
2024-02-20 16:39:58 +0100

    Create blogpage

--- src/app/pages/blogs-page/blogs-page.component.ts
+++ src/app/pages/blogs-page/blogs-page.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogCaption } from './interfaces/blog-caption-interface';
 
 @Component({
   selector: 'app-blogs-page',
   templateUrl: './blogs-page.component.html',
-  styleUrl: './blogs-page.component.scss'
+  styleUrl: './blogs-page.component.scss',
 })
-export class BlogsPageComponent {
+export class BlogsPageComponent implements OnInit {
+  blogs: BlogCaption[] = [];
 
+  ngOnInit(): void {
+    this.blogs.push(
+      {
+        title: '47 Bachelor Party Ideas for Every Groom',
+        text: 'Fill your bachelor party with groom-approved activities. From weekend getaways to nighttime options, explore the best and most unique bachelor party ideas.',
+        url: 'https://www.zola.com/expert-advice/bachelor-party-ideas',
+        img: 'assets/blogs/bachelor.jpg',
+      },
+      {
+        title: '10 Tips for Outdoor Adventure Vacations',
+        text: 'In recent years, wellness trips have become increasingly popular, with many travelers seeking adventure vacations that offer a combination of physical activity, natural beauty and personal growth.',
+        url: 'https://vacayou.com/magazine/top-10-tips-outdoor-adventure-vacations/',
+        img: 'assets/blogs/adventure.jpg',
+      },
+      {
+        title: 'Top 20 Rainy Day Activities',
+        text: ' We’ve put together a list of 20 kid-approved activities to combat that inevitable boredom, or you can just store them in your back pocket and save them for a rainy day.',
+        url: 'https://sunshinehouse.com/blog/top-20-rainy-day-activities/',
+        img: 'assets/blogs/rain.jpg',
+      },
+      {
+        title: 'Best Indoor Activities for Adults of All Ability Levels',
+        text: 'Indoor activities for older adults are varied and can include anything from gentle exercises such as Tai Chi, yoga, and Pilates, to games and puzzles, reading, art projects, and even gardening.',
+        url: 'https://www.considracare.com/indoor-activities-for-adults/',
+        img: 'assets/blogs/puzzle.jpg',
+      },
+      {
+        title: '25 Fun & Easy Dinners to Make If You’re Bored',
+        text: 'Cooking at home is one of those things that’s truly great if you enjoy cooking and is truly miserable if you don’t. ',
+        url: 'https://theeverygirl.com/dinners-to-make-if-youre-bored-with-cooking-at-home/',
+        img: 'assets/blogs/cooking.jpg',
+      }
+    );
+  }
+
+  onClick(blogUrl: string) {
+    window.open(blogUrl);
+  }
 }
---
+++ src/app/pages/blogs-page/interfaces/blog-caption-interface.ts
@@ -0,0 +1,6 @@
+export interface BlogCaption {
+  title: string;
+  text: string;
+  url: string;
+  img: string;
+}
